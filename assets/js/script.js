8d2fa38b9e97944f3689d8dff199682ca657b374
GregNasir <63434657+GregNasir@users.noreply.github.com>
2024-04-25 02:26:03 -0400

    got data to print to page from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,62 @@
 const issueContainer = document.getElementById('issues');
 const fetchButton = document.getElementById('fetch-button');
+const apiKeyEpl = "1758fda19dmshcb7e9f9e8e12fecp187acejsn16d775cbbb61";
 
 function getApi() {
-  const requestUrl = 'https://api.github.com/repos/IBM/clai/issues?per_page=5';
+  const params = {
+    i: '90a218c1eac4c020',
+    l: 'en_US'
+  };
 
-  fetch(requestUrl)
+  // Serialize params object into a query string
+  const queryString = Object.keys(params)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(params[key]))
+    .join('&');
+
+  // Append query string to the request URL
+  const requestUrl = 'https://soccer-football-info.p.rapidapi.com/matches/view/basic/?' + queryString;
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': apiKeyEpl,
+      'X-RapidAPI-Host': 'soccer-football-info.p.rapidapi.com'
+    }
+  };
+
+  fetch(requestUrl, options)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data);
-      for (let i = 0; i < data.length; i++) {
-        const userName = document.createElement('h3');
-        const issueTitle = document.createElement('p');
-        userName.textContent = data[i].user.login;
-        issueTitle.textContent = data[i].title;
-        issueContainer.append(userName);
-        issueContainer.append(issueTitle);
-      }
+
+      // Clear previous content
+      issueContainer.innerHTML = '';
+
+      console.log(data.result[0].referee.name);
+
+      const result = document.createElement('p');
+      result.textContent = data.result[0].referee.name;
+      issueContainer.appendChild(result);
+
+      // Check if data is available
+//       if (data && data.length > 0) {
+//         const refereeName = document.createElement('h3');
+//         refereeName.textContent = data.length; // Accessing the referee name from the first result
+//         issueContainer.appendChild(refereeName);
+//         console.log(data.result);
+//       } else {
+//         console.log('No data available');
+//       }
+//     })
+//     .catch(function (error) {
+//       console.error('Error fetching data:', error);
     });
-}
+ }
+
 fetchButton.addEventListener('click', getApi);
+
+
+
+
